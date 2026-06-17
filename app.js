1a27488e77b1cecbe810531fdac315f5b93b86a4
Your Name <huy484820@gmail.com>
2026-06-17 08:16:56 +0700

    Configure Vercel deployment

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use("/", orderRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
